fded0c0dad92da047753437dc8e89daa40c95132
Vino <vinofernando84@gmail.com>
2026-09-09 20:13:47 +0700

    add authprovider

---
+++ article-app/app/providers/AuthProvider.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+
+type User = {
+  id: number;
+  name: string;
+  email: string;
+};
+
+interface AuthContextType {
+  user: User | null;
+  setUser: React.Dispatch<React.SetStateAction<User | null>>;
+  isLoading: boolean;
+}
+
+const AuthContext = createContext<AuthContextType | null>(null);
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const getCurrentUser = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/api/auth/profile", {
+          method: "GET",
+          credentials: "include",
+        });
+
+        if (!response.ok) {
+          setUser(null);
+          return;
+        }
+
+        const data = await response.json();
+
+        setUser(data.user.payload);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Failed to get current user:", error);
+        setUser(null);
+        setIsLoading(false);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getCurrentUser();
+  }, []);
+  return (
+    <AuthContext.Provider value={{ user, setUser, isLoading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
